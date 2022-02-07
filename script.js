7c80ecdb5379143bc5c6dcbf6dd879a008a74fab
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-02-07 22:47:54 +0000

    fixed working of nav-menu button

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 window.onload = () => {
     const menuBtn = document.querySelector(".btn-menu");
-
+    const navLists =  document.querySelector(".navs");
     menuBtn.onclick = toggle;
-    window.onclick = (e) => {
-        if (!e.target.matches(".btn-menu")) {
-            let dropdowns = document.querySelector(".nav-list");
-            for (let i = 0; i < dropdowns.length; i++) {
-                const openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains("active")) {
-                    openDropdown.classList.remove("active");
-                }
-            }
-        }
-    }
-
     function toggle() {
-        document.querySelector(".nav-list").classList.toggle("active");
+       navLists.classList.toggle("active");
     }
-
 }
